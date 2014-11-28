e6165c2ad40deb209b649e667b2d41b9a9c84fa4
LPanda <376810857@qq.com>
2014-11-28 11:54:04 +0800

    no message

---
+++ JQLife/JQLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JQLife
+//
+//  Created by LPanda on 14-11-28.
+//  Copyright (c) 2014年 LPanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JQLife/JQLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JQLife
+//
+//  Created by LPanda on 14-11-28.
+//  Copyright (c) 2014年 LPanda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
